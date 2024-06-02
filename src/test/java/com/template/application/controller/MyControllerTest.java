3368a49f94db6046644334b01c7b42d44eaf00fc
techtrials-github-admin <techtrials+github-admin@pm.me>
2024-06-02 09:26:40 -0400

    cleanup and correction

--- src/test/java/com/template/application/controller/MyControllerTest.java
+++ src/test/java/com/template/application/controller/MyControllerTest.java
@@ -2,7 +2,6 @@ package com.template.application.controller;
 
 import com.template.application.core.Logic;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Tags;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
